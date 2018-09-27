3815209398105cab717ef4ba0469c162b1044ea2
duong17020661 <43227742+duong17020661@users.noreply.github.com>
2018-09-27 10:17:00 +0700

    Update Fraction.java

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -26,47 +26,50 @@ public class Fraction {
 
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
-        this.nume=numerator;
-        this.deno=denominator;
+            this.nume=numerator;
+            this.deno=denominator;
+    }
+    public Fraction(Objects obj){
+        
     }
 
-    public week2.Fraction add(week2.Fraction other) {
+    public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
 
         int m = this.deno*other.deno;
         int t = this.nume*other.deno+this.deno*other.nume;
         int ucln = task1.gcd(t,m);
-        week2.Fraction sum = new week2.Fraction(t/ucln,m/ucln);
+        Fraction sum = new Fraction(t/ucln,m/ucln);
 
         return sum;
     }
 
-    public week2.Fraction subtract(week2.Fraction other) {
+    public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
         int m = this.deno*other.deno;
         int t = this.nume*other.deno-this.deno*other.nume;
         int ucln = task1.gcd(t,m);
-        week2.Fraction subtract = new week2.Fraction(t/ucln,m/ucln);
+        Fraction subtract = new Fraction(t/ucln,m/ucln);
 
         return subtract;
     }
 
-    public week2.Fraction multiply(week2.Fraction other) {
+    public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
         int m = this.deno*other.deno;
         int t = this.nume*other.nume;
         int ucln = task1.gcd(t,m);
-        week2.Fraction mul = new week2.Fraction(t/ucln,m/ucln);
+        Fraction mul = new Fraction(t/ucln,m/ucln);
 
         return mul;
     }
 
-    public week2.Fraction divide(week2.Fraction other) {
+    public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
         int m = this.deno*other.nume;
         int t = this.nume*other.deno;
         int ucln = task1.gcd(t,m);
-        week2.Fraction div = new week2.Fraction(t/ucln,m/ucln);
+        Fraction div = new Fraction(t/ucln,m/ucln);
 
         return div;
     }
@@ -81,15 +84,15 @@ public class Fraction {
 
     public static void main(String[] args) {
 
-        week2.Fraction ps1 = new week2.Fraction(2,4);
-        week2.Fraction ps2 = new week2.Fraction(2,4);
-        week2.Fraction sum = ps1.add(ps2);
+        Fraction ps1 = new Fraction(1,2);
+        Fraction ps2 = new Fraction(2,4);
+        Fraction sum = ps1.add(ps2);
         System.out.println(sum.nume + "/" + sum.deno);
-        week2.Fraction sub = ps1.subtract(ps2);
+        Fraction sub = ps1.subtract(ps2);
         System.out.println(sub.nume + "/" + sub.deno);
-        week2.Fraction mul = ps1.multiply(ps2);
+        Fraction mul = ps1.multiply(ps2);
         System.out.println(mul.nume + "/" + mul.deno);
-        week2.Fraction div = ps1.divide(ps2);
+        Fraction div = ps1.divide(ps2);
         System.out.println(div.nume + "/" + div.deno);
         System.out.println(ps1.equals(ps2));
     }
