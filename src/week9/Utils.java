a2af697f7e4d3697b8e6d5995c1cd4c64fdea398
duong17020661 <duong2199>
2018-11-08 10:28:06 +0700

    aaaa

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -1,13 +1,17 @@
 package week9;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Utils {
     public static String readContentFromFile(String path) throws IOException {
         File file = new File(path);
         FileReader reader = new FileReader(file);
+        InputStreamReader reader1 = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(reader);
-        String s = in.readLine();
+        String s;
+        s = new String(Files.readAllBytes(Paths.get(path)));
         in.close();
         return s;
     }
@@ -18,7 +22,6 @@ public class Utils {
         String s = "Hello";
         out.println(s);
         out.close();
-
     }
     public static void appendContentToFile(String path) throws IOException {
         File file = new File(path);
@@ -35,6 +38,9 @@ public class Utils {
                 if (file.getName().equals(fileName)) {
                     System.out.println(file.getAbsolutePath());
                 }
+                else {
+                    System.out.println("khong ton tai");
+                }
             }
             File[] listFile = file.listFiles();
             if (listFile != null) {
@@ -42,17 +48,26 @@ public class Utils {
                     findFileByName(f.getAbsolutePath(), fileName);
                 }
             }
-        } else {
+        }
+        else {
+            try {
+                file.createNewFile();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
             System.out.println("source không tồn tại");
         }
         return file;
     }
 
     public static void main(String[] args) throws IOException {
+
         String filename1 = "Hawaii.txt";
-        String filename2 = "data.txt";
-        System.out.println(readContentFromFile(filename1));
+        String filename2 = "data2.txt";
         writeContentToFile(filename2);
         appendContentToFile(filename2);
+        System.out.println(readContentFromFile(filename2));
+        findFileByName("folder","data.txt");
+        System.out.println(findFileByName("New folder","datatxt"));
     }
 }
