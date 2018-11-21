5279bd787cd5224794101ade271a7e4eccfeabbe
duong17020661 <43227742+duong17020661@users.noreply.github.com>
2018-11-21 21:09:38 +0700

    update

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,41 @@
 package week11;
 
-public class Task1 {
+import java.util.Collections;
+
+public class Task1<T> {
+    private T value;
+    public Task1(){
+        value = null;
+    }
+    public Task1(T value){
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public static <T extends Comparable> void Sort(T[] a){
+        for(int i = 0 ; i < a.length - 1 ; i++){
+            for (int j = 0 ; j < a.length - 1 ; j++){
+                if(a[j].compareTo(a[j+1])>0){
+                    T temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String[] word = {"dasdd", "ba", "asd", "bc"};
+        Sort(word);
+        for(int i = 0 ; i < word.length ; i++){
+            System.out.println(word[i]);
+        }
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,24 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public static <T extends Comparable> T Max(ArrayList<T> a){
+        T max = a.get(0);
+        for (int i = 0 ; i < a.size() ; i++){
+            if(max.compareTo(a.get(i))<0) max = a.get(i);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<Integer>();
+        integers.add(1);
+        integers.add(15);
+        integers.add(14);
+        integers.add(13);
+        integers.add(11);
+        integers.add(12);
+        System.out.println(Max(integers));
+    }
 }
