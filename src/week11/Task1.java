7b6a25e12660f1c4828e3027bc7d6acb045f485b
duong17020661 <duong.coi.bs.1104@gmail.com>
2018-11-22 08:13:23 +0700

    up

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -3,22 +3,6 @@ package week11;
 import java.util.Collections;
 
 public class Task1<T> {
-    private T value;
-    public Task1(){
-        value = null;
-    }
-    public Task1(T value){
-        this.value = value;
-    }
-
-    public T getValue() {
-        return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
-    }
-
     public static <T extends Comparable> void Sort(T[] a){
         for(int i = 0 ; i < a.length - 1 ; i++){
             for (int j = 0 ; j < a.length - 1 ; j++){
