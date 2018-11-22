b2bb64b390347d2a1b0818b80d82e0f1a7e34678
duong17020661 <duong.coi.bs.1104@gmail.com>
2018-11-22 08:48:34 +0700

    up

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,7 +1,5 @@
 package week11;
 
-import java.util.Collections;
-
 public class Task1<T> {
     public static <T extends Comparable> void Sort(T[] a){
         for(int i = 0 ; i < a.length - 1 ; i++){
@@ -17,9 +15,14 @@ public class Task1<T> {
     }
     public static void main(String[] args) {
         String[] word = {"dasdd", "ba", "asd", "bc"};
+        Integer[] num = {1,1,2,3,5,7,7};
         Sort(word);
         for(int i = 0 ; i < word.length ; i++){
             System.out.println(word[i]);
+        }
+        Sort(num);
+        for(int i = 0 ; i < num.length ; i++){
+            System.out.println(num[i]);
         }
     }
 }
